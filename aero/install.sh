a57277ea8ccbc6b91b16f3c5dc1a14f0ab7144c6
trollmeight <trollm8@proton.me>
2026-05-03 18:01:45 -0500

    Update install.sh

--- aero/install.sh
+++ aero/install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-if [ "$(id -u)" -ne 0 ]; then
-   echo "This script must be run with root privileges."
-   exit 1
-fi
-
 curl -o /usr/local/bin/$USER/aero.sh http://trollmeight.me/aero/aero.sh
+mkdir /usr/local/bin/$USER/aero_external
+curl -o /usr/local/bin/$USER/aero_external/update.sh http://trollmeight.me/aero/aero_external/update.sh
