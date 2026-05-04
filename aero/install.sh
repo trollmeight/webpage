97cc8e4fe61d3b166d3acb30b23acb347a2150ae
trollmeight <trollm8@proton.me>
2026-05-03 19:34:54 -0500

    Update install.sh

--- aero/install.sh
+++ aero/install.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 curl -o /usr/local/bin/$USER/aero.sh http://trollmeight.me/aero/aero.sh
-mkdir /usr/local/bin/$USER/aero_external
-curl -o /usr/local/bin/$USER/aero_external/update.sh http://trollmeight.me/aero/aero_external/update.sh
